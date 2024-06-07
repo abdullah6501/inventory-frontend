18bfeab038a00552ca44f950a952620284afdd2c
abdullah <abdullahsm0111@gmail.com>
2024-06-07 12:36:30 +0530

    ip change

--- src/environment/envirinment.ts
+++ src/environment/envirinment.ts
@@ -1,7 +1,8 @@
 // environment.ts
 export const environment = {
   production: false,
-  INVENTORY_BASEURL: 'http://192.168.0.140:1023'
+  // INVENTORY_BASEURL: 'http://192.168.0.140:1023'
+  INVENTORY_BASEURL: 'http://192.168.0.155:1023'
   // INVENTORY_BASEURL: 'http://192.168.0.140:1022'
   // INVENTORY_BASEURL: 'http://192.168.0.140:3003'
 };
