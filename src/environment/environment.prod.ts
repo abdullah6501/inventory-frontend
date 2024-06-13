031b8df3ad03e102675285d863055f19b47b88bc
abdullah6501 <87361506+abdullah6501@users.noreply.github.com>
2024-06-13 10:54:10 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // INVENTORY_BASEURL: 'http://192.168.0.140:1023'
-  INVENTORY_BASEURL: 'http://192.168.0.167:1023'
+  INVENTORY_BASEURL: 'http://192.168.0.140:1023'
   // INVENTORY_BASEURL: 'http://192.168.0.140:1022'
   // INVENTORY_BASEURL: 'http://192.168.0.140:3003'
 };
